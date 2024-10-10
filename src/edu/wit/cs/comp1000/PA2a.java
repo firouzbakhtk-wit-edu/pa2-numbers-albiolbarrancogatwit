38e74ef1bdbe6f30bb335dd03cdb43ed11610755
Gonzalo Juan Albiol Barranco <albiolbarrancog@wit.edu>
2024-10-10 14:46:35 -0400

    Finished part a and part b, completely documented, with all tests passed

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,116 @@
 package edu.wit.cs.comp1000;
-
-// TODO: document this class
+import java.util.Scanner;
+/**
+ * Solution to the 2nd programming assignment, part a.
+ */
 public class PA2a {
 
-	// TODO: document this function
+	/**
+	 * When it runs, it asks the user for 5 whole numbers in the terminal.
+	 * After getting them, it outputs the sum of the positive, negative, and all numbers,
+	 * and then the average of the positive, negative, and all numbers to the terminal.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner scanner = new Scanner(System.in);
+		System.out.printf("Enter five whole numbers: ");
+		int sum = 0;
+		int sumpos = 0;
+		int sumneg = 0;
+		int cntpos = 0;
+		int cntneg = 0;
+		int a = 0;
+		a = scanner.nextInt();
+		sum = sum + a;
+		if (a>0) {
+			sumpos = sumpos + a;
+			cntpos = cntpos + 1;
+		}
+		else {
+			sumneg = sumneg + a;
+			cntneg = cntneg + 1;
+		}
+		a = scanner.nextInt();
+		sum = sum + a;
+		if (a>0) {
+			sumpos = sumpos + a;
+			cntpos = cntpos + 1;
+		}
+		else {
+			sumneg = sumneg + a;
+			cntneg = cntneg + 1;
+		}
+		a = scanner.nextInt();
+		sum = sum + a;
+		if (a>0) {
+			sumpos = sumpos + a;
+			cntpos = cntpos + 1;
+		}
+		else {
+			sumneg = sumneg + a;
+			cntneg = cntneg + 1;
+		}
+		a = scanner.nextInt();
+		sum = sum + a;
+		if (a>0) {
+			sumpos = sumpos + a;
+			cntpos = cntpos + 1;
+		}
+		else {
+			sumneg = sumneg + a;
+			cntneg = cntneg + 1;
+		}
+		a = scanner.nextInt();
+		sum = sum + a;
+		if (a>0) {
+			sumpos = sumpos + a;
+			cntpos = cntpos + 1;
+		}
+		else {
+			sumneg = sumneg + a;
+			cntneg = cntneg + 1;
+		}
+		double avgpos;
+		double avgneg;
+		if (cntpos>0) {
+		avgpos = sumpos / (cntpos + 0.0);
+		}
+		else {
+			avgpos = 0.0;
+		}
+		if (cntneg>0) {
+			avgneg = sumneg / (cntneg + 0.0);
+			}
+			else {
+				avgneg = 0.0;
+			}
+		double avg = sum / 5.0;
+		if (cntpos== 1) {
+			System.out.printf("The sum of the %d positive number: %d%n", cntpos, sumpos);
+		}
+		else {
+			System.out.printf("The sum of the %d positive numbers: %d%n", cntpos, sumpos);
+		}
+		if (cntneg== 1) {
+			System.out.printf("The sum of the %d non-positive number: %d%n", cntneg, sumneg);
+		}
+		else {
+			System.out.printf("The sum of the %d non-positive numbers: %d%n", cntneg, sumneg);
+		}
+		System.out.printf("The sum of the 5 numbers: %d%n", sum);
+		if (cntpos== 1) {
+			System.out.printf("The average of the %d positive number: %.2f%n", cntpos, avgpos);
+		}
+		else {
+			System.out.printf("The average of the %d positive numbers: %.2f%n", cntpos, avgpos);
+		}
+		if (cntneg== 1) {
+			System.out.printf("The average of the %d non-positive number: %.2f%n", cntneg, avgneg);
+		}
+		else {
+			System.out.printf("The average of the %d non-positive numbers: %.2f%n", cntneg, avgneg);
+		}
+		System.out.printf("The average of the 5 numbers: %.2f%n", avg);	
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,42 @@
 package edu.wit.cs.comp1000;
+import java.lang.Math;
+import java.util.Scanner;
 
-// TODO: document this class
+/**
+ * Solution to the 2nd programming assignment, part b.
+ */
 public class PA2b {
 
-	// TODO: document this function
+	/**
+	 * When the code runs, it asks the user to input a, b and c.
+	 * Then, it determines whether the discriminant is real or not.
+	 * If it isn't, it just states that the roots are imaginary.
+	 * Otherwise, it then calculates the roots.
+	 * If the roots are equal, it just prints out "Root: " and then the root.
+	 * Otherwise, it prints "Roots:" and then both roots.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner scanner = new Scanner(System.in);
+		System.out.printf("Enter a b c: ");
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
+		double c = scanner.nextDouble();
+		double discriminant = (b*b)-(4*a*c);
+		if (discriminant >= 0) {
+			double root1 = (((-b) - Math.sqrt(discriminant))/(2*a));
+			double root2 = (((-b) + Math.sqrt(discriminant))/(2*a));
+			if (root1 == root2) {
+				System.out.printf("Root: %.2f%n",root1);
+			}
+			else {
+				System.out.printf("Roots: %.2f, %.2f%n", root1, root2);
+			}
+		}
+		else {
+			System.out.printf("Roots: imaginary%n");
+		}
+		
 	}
 
 }
